d397a73a1dcbbb3dd3a8d97fa1fb766715678600
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-28 20:29:14 +0100

    format util.cpp
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/util.cpp
+++ src/util.cpp
@@ -3,7 +3,7 @@
  * Github: https://www.github.com/0x4248/cppBASIC
  * Licence: GNU General Public License v3.0
  * By: 0x4248
-*/
+ */
 
 #include <iostream>
 #include <string>
@@ -12,22 +12,26 @@
 
 #include "util.h"
 
-std::vector<std::string> tokenize(const std::string& str) {
+std::vector<std::string> tokenize(const std::string &str)
+{
     std::vector<std::string> tokens;
     std::istringstream iss(str);
     std::string token;
-    
-    while (iss >> token) {
+
+    while (iss >> token)
+    {
         tokens.push_back(token);
     }
-    
+
     return tokens;
 }
 
-
-int find_variable(std::string variable, std::vector<std::string> variables){
-    for(int i = 0; i < variables.size(); i++){
-        if(variables[i] == variable){
+int find_variable(std::string variable, std::vector<std::string> variables)
+{
+    for (int i = 0; i < variables.size(); i++)
+    {
+        if (variables[i] == variable)
+        {
             return i;
         }
     }
